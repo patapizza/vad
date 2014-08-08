3640e6c87ef77bccb61d9ad200e1d77bb734be9f
patapizza <patapizza@gmail.com>
2014-08-07 19:58:48 -0700

    removing comments

--- plot.py
+++ plot.py
@@ -5,16 +5,6 @@ import matplotlib.pyplot as plt
 import json
 
 if __name__ == '__main__':
-    '''data = None
-    with open('data.json', 'r') as f:
-        data = json.loads(f.read())
-    if data:
-        n = len(data['stream'])
-        plt.figure(1)
-        plt.plot(numpy.linspace(0, float(n) / 16000, num=n), data['stream'])
-        plt.vlines(data['speech'], data['min_freq'], data['max_freq'], 'g')
-        plt.vlines(data['noise'], data['min_freq'], data['max_freq'], 'r')
-        plt.show(block=False)'''
     data = numpy.load('data.npy')
     n = len(data[0])
     plt.figure(1)
